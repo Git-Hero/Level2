5c11a1cc9ae19c87652d2b205c0b0dfcf9a3cbbc
Krishna Kumar <krishnaklooper@gmail.com>
2016-04-03 22:22:48 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
 # four function calculator in python
 
-def add(a, b)
+def add(a, b):
     return a+b
 
 def subtract (a,b):
     return a-b
 
-def multiply (a,b)
-    return a*b;
+def multiply (a,b):
+    return a*b
 
 def divide (a,b):
-	try;
+	try:
 		return a/b
-	except ZeroDivisionError
-		print "Division not possible ", end=' ');
+	except ZeroDivisionError as detail:
+		print 'Division not possible:',detail 
 		return False
 
 def calculate(a, b, op):
-    if (op == '+'):
-        print (add(a, b));
-    elif (op == '-'):
-        print (subtract(a, b));
-    elif (op =='*'):
-        print (multiply(a, b));
-    elif (op == '/'):
-        print (divide(a, b))
+    if op == '+':
+        print add(a, b)
+    elif op == '-':
+        print subtract(a, b)
+    elif op =='*':
+        print multiply(a, b)
+    elif op == '/':
+        print divide(a, b)
     else:
-        print ("invalid operator")
+        print "invalid operator"
 
-'''
-txt = input()           # format: num1 operator num2 (separated by space)
-txt = txt.split("")
-a = int(txt[0])
-b = int(txt[2]);
-op = str(txt[1])
-calculate(a, b, op)
+
+
+if __name__ == "__main__": 
+     txt = raw_input()           # format: num1 operator num2 (separated by space)
+     txt = txt.split(" ")
+     a = int(txt[0])
+     b = int(txt[2])
+     op = str(txt[1])
+     calculate(a, b, op)
